b9af5bb78e79a139c2cac511e566e7d2207a4596
p-karari <kararikelvin2002@gmail.com>
2023-04-30 00:00:07 +0300

    adds logic behind various buttons in the website

---
+++ ap.js
@@ -0,0 +1,40 @@
+const btns = document.querySelectorAll(".explore_field_btn")
+let header = document.querySelectorAll(".card_title")
+
+function buy() {
+    header[0].textContent = "create an account"
+    header[1].textContent = "morgage calculator"
+    header[2].textContent = "explore  available properties"
+    header[3].textContent = "contact an agent"
+}
+
+
+function buy() {
+    header[0].textContent = "create an account"
+    header[1].textContent = "morgage calculator"
+    header[2].textContent = "explore  available properties"
+    header[3].textContent = "contact an agent"
+}
+
+
+function buy() {
+    header[0].textContent = "create an account"
+    header[1].textContent = "morgage calculator"
+    header[2].textContent = "explore  available properties"
+    header[3].textContent = "contact an agent"
+}
+
+
+function buy() {
+    header[0].textContent = "create an account"
+    header[1].textContent = "morgage calculator"
+    header[2].textContent = "explore  available properties"
+    header[3].textContent = "contact an agent"
+}
+
+btns[0].addEventListener('click' , function(){
+    buy()
+})
+
+
+console.log(btns)
